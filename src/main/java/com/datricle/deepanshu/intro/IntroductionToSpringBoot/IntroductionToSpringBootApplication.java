b62d2af6aaac98bec32effcf11cac31a80790edb
Deepanshu-sehgal <deepanshusehgal.112@gmail.com>
2024-06-29 13:26:09 +0530

    1.2 Setting UP

---
+++ src/main/java/com/datricle/deepanshu/intro/IntroductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.datricle.deepanshu.intro.IntroductionToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
+		System.out.println("Application is running");
+	}
+
+}
---
+++ src/test/java/com/datricle/deepanshu/intro/IntroductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datricle.deepanshu.intro.IntroductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
